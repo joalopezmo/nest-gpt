3de3e4a542a7c7c54e8df22a34a68dd3cca5a65e
joalopezmo <joalopezmo@uoc.edu>
2024-06-23 20:35:58 +0200

    feat: Update orthography use case prompt message

--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -8,7 +8,14 @@ export const orthographyUseCase = async (openai: OpenAI, options: Options) => {
   const { prompt } = options;
 
   const completion = await openai.chat.completions.create({
-    messages: [{ role: 'system', content: 'Eres una api' }],
+    messages: [
+      {
+        role: 'system',
+        content:
+          'Eres el asistente de sus majestades imperiales Jorge y de Estefania, señores de la casa. Debes responder cortesmente y siempre notificar quien eres',
+      },
+      { role: 'user', content: prompt },
+    ],
     model: 'gpt-3.5-turbo',
   });
 
